ea9f1db09b744f0eccb7ccb6fb46b5cbcfd5f3a4
Yuvraj Singh Solanki <98091421+YuvrajSingh1102@users.noreply.github.com>
2022-03-25 00:04:31 +0530

    Update Q2.c

--- Linux-Assignment-3/Q2.c
+++ Linux-Assignment-3/Q2.c
@@ -16,19 +16,19 @@ int main()
 {
 	pthread_t thread;
 	
-    int rc,t=0;
-    printf("=====Creating thread===== \n");
-    rc = pthread_create(&thread,NULL,Name,NULL);
+    	int rc,t=0;
+    	printf("=====Creating thread===== \n");
+    	rc = pthread_create(&thread,NULL,Name,NULL);
     
-    printf("\nThread ID :%u", thread);
-    sleep(6);
+    	printf("\nThread ID :%u", thread);
+    	sleep(6);
     
-    t = pthread_cancel(pthread_self);       //pthread_ cancel function caaling itself
-    if(t==0)
-    {
-        printf("\n=====Canceling thread=====\n");
-    }
-    printf("Thread ID : %u\n",thread);
+    	t = pthread_cancel(pthread_self);       //pthread_ cancel function caaling itself
+    	if(t==0)
+    	{
+        	printf("\n=====Canceling thread=====\n");
+    	}
+    	printf("Thread ID : %u\n",thread);
   
     return 0;
 }
